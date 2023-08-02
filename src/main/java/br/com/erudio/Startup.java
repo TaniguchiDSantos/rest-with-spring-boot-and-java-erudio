194bdbd2d76f2e905f5a419a6a19126414e4803c
Taniguchi Santos <taniguchi.santos@hotmail.com>
2023-08-01 21:24:10 -0300

    Primeiros Passos

--- src/main/java/br/com/erudio/Startup.java
+++ src/main/java/br/com/erudio/Startup.java
@@ -1,13 +1,13 @@
-package com.example.restwithspringbootandjavaerudio;
+package br.com.erudio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestWithSpringBootAndJavaErudioApplication {
+public class Startup {
 
     public static void main(String[] args) {
-        SpringApplication.run(RestWithSpringBootAndJavaErudioApplication.class, args);
+        SpringApplication.run(Startup.class, args);
     }
 
 }
